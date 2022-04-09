34a11d454e2a7b53cf670aa47a258c066364fdf4
Lee Xiang Wei <688247+xwlee@users.noreply.github.com>
2022-04-09 15:22:47 +0800

    Configure bucket & distribution

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
-# Welcome to your CDK TypeScript project
+# cdk-s3-cloudfront
 
-This is a blank project for TypeScript development with CDK.
+A CDK stack to create a `CloudFront Distribution` that serves a private `S3 Bucket` content.
+Users must access the image using `CloudFront URL`.
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
-
-## Useful commands
-
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+![Diagram](diagram.png)
--- lib/cdk-s3-cloudfront-stack.ts
+++ lib/cdk-s3-cloudfront-stack.ts
@@ -1,16 +1,28 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
 
 export class CdkS3CloudfrontStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, "Bucket", {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkS3CloudfrontQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const distribution = new cloudfront.Distribution(this, "Distribution", {
+      defaultBehavior: { origin: new origins.S3Origin(bucket) }, // Automatically creates an Origin Access Identity
+    });
+
+    const deployment = new s3deploy.BucketDeployment(this, "BucketDeployment", {
+      sources: [s3deploy.Source.asset("assets")],
+      destinationBucket: bucket,
+      retainOnDelete: false,
+      distribution, // Automatically invalidate distribution's edge cache
+    });
   }
 }
